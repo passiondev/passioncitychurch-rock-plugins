a487b22ea56f6ecdeb0f0f2f4fafb57f095b00aa
blbynum <ben@bbynum.com>
2020-07-31 15:54:29 -0400

    Add custom Person Signal badge and update passion DLL

---
+++ com.passioncitychurch/Badge/Component/TopPersonSignalPassion.cs
@@ -0,0 +1,78 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+using Rock;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Badge;
+
+namespace com.passioncitychurch.Badge.Component
+{
+    /// <summary>
+    /// Shows the top person signal.
+    /// </summary>
+    [Description("Custom version of Signal badge that shows text: \"Restrictions | See Leadership Team\".")]
+    [Export( typeof( BadgeComponent ) )]
+    [ExportMetadata( "ComponentName", "Passion - Top Person Signal" )]
+
+    public class TopPersonSignalPassion : BadgeComponent
+    {
+        /// <summary>
+        /// Determines of this badge component applies to the given type
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public override bool DoesApplyToEntityType( string type )
+        {
+            return type.IsNullOrWhiteSpace() || typeof( Person ).FullName == type;
+        }
+
+        /// <summary>
+        /// Renders the specified writer.
+        /// </summary>
+        /// <param name="badge">The badge.</param>
+        /// <param name="writer">The writer.</param>
+        public override void Render( BadgeCache badge, System.Web.UI.HtmlTextWriter writer )
+        {
+            if ( Person == null )
+            {
+                return;
+            }
+
+            if ( !string.IsNullOrWhiteSpace( Person.TopSignalColor ) && Person.Signals.Count > 0 )
+            {
+                writer.Write( string.Format(@"
+<div class='badge badge-signal badge-id-{0}' data-toggle='tooltip' title='Restrictions | See Leadership Team'>
+    <div class='badge-content' style='color: {1};'>
+        <i class='fa fa-flag badge-icon'></i>
+        <span class='signal'>{2}</span>
+    </div>
+</div>",
+                    badge.Id,
+                    Person.TopSignalColor,
+                    Person.Signals.Count,
+                    Person.NickName,
+                    "signal".PluralizeIf( Person.Signals.Count != 1 ),
+                    string.Join( ", ", Person.Signals.Select( s => s.SignalType.Name.EncodeHtml() ) ) ) );
+            }
+        }
+    }
+}
